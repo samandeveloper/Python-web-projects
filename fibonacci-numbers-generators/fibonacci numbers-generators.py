0904b64478d2d55bd9d21553ef4aaeb4e8f441f3
Saman M. Mohaghegh <63525638+samandeveloper@users.noreply.github.com>
2022-11-25 00:08:49 -0700

    upload files

---
+++ fibonacci-numbers-generators/fibonacci numbers-generators.py
@@ -0,0 +1,39 @@
+#Fibonacci Numbers-Generators
+
+def fib(num):
+  a=0
+  second_number=1
+  while True:
+    yield first_number
+    first_number=second_number
+    second_number=first_number+second_number
+    # break
+for item in range(20):
+  print(item)
+
+#Here is an example generator which calculates fibonacci numbers:
+# generator version
+
+def fib(number):
+    a =  0
+    b = 1
+    for i in range(number):
+        yield a
+        temp = a
+        a = b
+        b = temp + b
+        
+for x in fib(20):
+    print(x)
+
+def fib2(number):
+    a =  0
+    b = 1
+    result = []
+    for i in range(number):
+        result.append(a)
+        temp = a
+        a = b
+        b = temp + b
+    return result
+print(fib2(100))
